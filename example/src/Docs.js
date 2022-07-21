face1ef758ca966d712f164dd3558aa2a7c3982f
Drazail <reza.y.3@gmail.com>
2022-07-21 23:50:45 +0300

    adding docs to example

--- example/src/Docs.js
+++ example/src/Docs.js
@@ -4,31 +4,36 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 
 export default function Docs() {
   return (
-    <div style={{ width: window.innerWidth / 1.5 }}>
+    <div style={{ width: window.innerWidth / 1.1 }}>
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Constants</Typography>
+          <Typography variant="h5">Constants</Typography>
         </AccordionSummary>
         <AccordionDetails>
-            <Typography  variant="h5">{`typeDef:`}</Typography>
-            <hr width="30%" align="left" />
+          <Typography variant="h6">{`typeDef:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
             <Typography variant="body2">{`HashAlgorithms : Record< string, string>`}</Typography>
             <Typography variant="body2">{`HmacAlgorithms : Record<string, string>;`}</Typography>
-            <hr width="100%" align="left" />
-            <Typography variant="h5">{`Example:`}</Typography>
-            <hr width="30%" align="left" />
-            <Typography variant="body2">{`import { CONSTANTS } from 'react-hash';`}</Typography>
-            <Typography variant="body2">{`const hashAlgorithm = CONSTANTS.HashAlgorithms.sha256;`}</Typography>
-            <Typography variant="body2">{`const hmacAlgorithm = CONSTANTS.HmacAlgorithms.HmacSHA512;`}</Typography>
-           
+          </Paper>
+          <br />
+          <Typography variant="h6">{`Example:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <code>
+              <Typography variant="body2">{`import { CONSTANTS } from 'react-hash';`}</Typography>
+              <Typography variant="body2">{`const hashAlgorithm = CONSTANTS.HashAlgorithms.sha256;`}</Typography>
+              <Typography variant="body2">{`const hmacAlgorithm = CONSTANTS.HmacAlgorithms.HmacSHA512;`}</Typography>
+            </code>
+          </Paper>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -37,13 +42,25 @@ export default function Docs() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>JS Hash</Typography>
+          <Typography variant="h5">JS Hash</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <Typography variant="h6">{`typeDef:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <Typography variant="body2">{`JSHash(message: string, algorithm: string):Promise<string>;`}</Typography>
+          </Paper>
+          <br />
+          <Typography variant="h6">{`Example:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <code>
+              <Typography variant="body2">{`import { JSHash, CONSTANTS } from "react-hash";`}</Typography>
+              <Typography variant="body2">{`JSHash("message", CONSTANTS.HashAlgorithms.sha256)`}</Typography>
+              <Typography variant="body2">{`.then(hash => console.log(hash))`}</Typography>
+              <Typography variant="body2">{`.catch(e => console.log(e));`}</Typography>
+            </code>
+          </Paper>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -52,13 +69,25 @@ export default function Docs() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>JS Hmac</Typography>
+          <Typography variant="h5">JS Hmac</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <Typography variant="h6">{`typeDef:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <Typography variant="body2">{`JSHmac(message: string, secret: string, algorithm: string): Promise<string>;`}</Typography>
+          </Paper>
+          <br />
+          <Typography variant="h6">{`Example:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <code>
+              <Typography variant="body2">{`import { JSHmac, CONSTANTS } from "react-hash";`}</Typography>
+              <Typography variant="body2">{`JSHmac("message", "SecretKey", CONSTANTS.HmacAlgorithms.HmacSHA256)`}</Typography>
+              <Typography variant="body2">{`.then(hash => console.log(hmac))`}</Typography>
+              <Typography variant="body2">{`.catch(e => console.log(e));`}</Typography>
+            </code>
+          </Paper>
         </AccordionDetails>
       </Accordion>
 
@@ -68,13 +97,30 @@ export default function Docs() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>useHash</Typography>
+          <Typography variant="h5">useHash</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <Typography variant="h6">{`typeDef:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <Typography variant="body2">{`useHash(
+              hmacAlgo?: string = "MD5",
+              initialMessage: ?string = "hello World",
+            ): [
+              hashed: string,
+              setMessage: (message: string) => Promise<void>,
+              setAlgo: (algo: string) => Promise<void>
+            ];`}</Typography>
+          </Paper>
+          <br />
+          <Typography variant="h6">{`Example:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <code>
+              <Typography variant="body2">{`import { useHash } from "react-hash";`}</Typography>
+              <Typography variant="body2">{`const [hashedMessage, setHashAlgo, setHashMessage] = useHash();`}</Typography>
+            </code>
+          </Paper>
         </AccordionDetails>
       </Accordion>
 
@@ -84,13 +130,32 @@ export default function Docs() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>useHmack</Typography>
+          <Typography variant="h5">useHmack</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <Typography variant="h6">{`typeDef:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <Typography variant="body2">{`useHmac(
+                hmacAlgo?: string = "HmacMD5",
+                initialMessage: ?string = "hello World",
+                initialSecret: ?string = "SecretKey"
+              ): [
+                hmac: string,
+                setMessage: (message: string) => Promise<void>,
+                setAlgo: (algo: string) => Promise<void>,
+                setSecret: (secret: string) => Promise<void>
+              ];`}</Typography>
+          </Paper>
+          <br />
+          <Typography variant="h6">{`Example:`}</Typography>
+          <br />
+          <Paper elevation={2} style={{ padding: 10 }}>
+            <code>
+              <Typography variant="body2">{`import { useHmac } from "react-hash";`}</Typography>
+              <Typography variant="body2">{`const [hmac, setHmacAlgo, setHmacMessage, setHmacSecret] = useHmac();`}</Typography>
+            </code>
+          </Paper>
         </AccordionDetails>
       </Accordion>
     </div>
